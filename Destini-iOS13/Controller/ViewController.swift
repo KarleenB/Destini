54bf007d51f5d55c801d3dde309204fa7304e50c
Branden Karleen <karleen@MacBook-Karleen.local>
2021-09-13 17:04:08 -0700

    Initial Commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+//step 5.5
+// to access everything in the StoryBrain struct I need to make an instance in order to reference it
+    var storyBrain = StoryBrain()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
     }
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
 
-
+//step 5.6
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+        
+    }
+    
+    
+    func updateUI () {
+        let storyChoice = storyBrain.stories[storyBrain.storyNumber]
+        
+        storyLabel.text = storyChoice.title
+        choice1Button.setTitle(storyChoice.choice1,for: .normal)
+        choice2Button.setTitle(storyChoice.choice2, for: .normal)
+        
+    }
+    
+    
 }
+
 
